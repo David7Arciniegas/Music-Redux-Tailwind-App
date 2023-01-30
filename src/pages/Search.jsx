3ec4a709d5f952be32ebabe8020d84e7073035aa
LuxFer <david7arciniegas@gmail.com>
2023-01-30 18:14:40 -0500

    Finished SearchBar Component

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import React from "react";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router-dom";
 
-import { Error, Loader, SongCard } from '../components';
-import { useGetSongsBySearchQuery } from '../redux/services/shazamCore';
+import { Error, Loader, SongCard } from "../components";
+import { useGetSongsBySearchQuery } from "../redux/services/shazamCore";
 
 const Search = () => {
   const { searchTerm } = useParams();
@@ -18,7 +18,9 @@ const Search = () => {
 
   return (
     <div className="flex flex-col">
-      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">Showing results for <span className="font-black">{searchTerm}</span></h2>
+      <h2 className="font-bold text-3xl text-white text-left mt-4 mb-10">
+        Showing results for <span className="font-black">{searchTerm}</span>
+      </h2>
 
       <div className="flex flex-wrap sm:justify-start justify-center gap-8">
         {songs.map((song, i) => (
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -17,15 +17,19 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
           getSongRelated: builder.query({ query: ({songid}) => `/v1/tracks/related?track_id=${songid}`}),
           getArtistDetails: builder.query({ query: (artistId) => `/v2/artists/details?artist_id=${artistId}`}),
           getSongsByCountry: builder.query({ query: (countryCode) => `v1/charts/country?country_code=${countryCode}` }),
+          getSongsBySearch: builder.query({ query: (searchTerm) => `v1/search/multi?search_type=SONGS_ARTISTS&query=${searchTerm}` }),
+          
         }),
     });
 
     export const{
+      
       useGetTopChartsQuery,
       useGetSongsByGenreQuery,
       useGetSongDetailsQuery,
       useGetSongRelatedQuery,
       useGetArtistDetailsQuery,
+      useGetSongsBySearchQuery,
       useGetSongsByCountryQuery
 
     } = shazamCoreApi;
